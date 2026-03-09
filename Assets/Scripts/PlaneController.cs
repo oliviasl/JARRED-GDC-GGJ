86eedada6cf1441692caf7184fc03cbb70cd694a
Roosevelt Sinker <66215548+rsinker@users.noreply.github.com>
2026-03-09 01:28:49 -0700

    added roll

--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -13,6 +13,7 @@ public class PlaneController : MonoBehaviour
     [SerializeField] private float maxFlySpeed;
     [SerializeField] private float speedChangeMagnitude = 5f;
     [SerializeField] private float turnSensitivity = 30f;
+    [SerializeField] private float rollSensitivity = 30f;
     [SerializeField, Range(1f, 20f)] private float rotationSmoothing = 5f;
 
     [SerializeField, ReadOnly] private float flySpeed;
@@ -46,11 +47,13 @@ public class PlaneController : MonoBehaviour
 
         float pitchInput = -normalizedOffset.y * turnSensitivity * 1.5f;
         float yawInput   =  normalizedOffset.x * turnSensitivity;
+        float rollInput = -Input.GetMoveInput().z * rollSensitivity;
 
         Quaternion pitch = Quaternion.AngleAxis(pitchInput * Time.fixedDeltaTime, Vector3.right);
-        Quaternion yaw   = Quaternion.AngleAxis(yawInput   * Time.fixedDeltaTime, Vector3.up);
+        Quaternion yaw   = Quaternion.AngleAxis(yawInput * Time.fixedDeltaTime, Vector3.up);
+        Quaternion roll = Quaternion.AngleAxis(rollInput * Time.fixedDeltaTime, Vector3.forward);
 
-        targetRotation *= pitch * yaw;
+        targetRotation *= pitch * yaw * roll;
         targetRotation.Normalize();
 
         Quaternion smoothedRotation = Quaternion.Slerp(rb.rotation, targetRotation, rotationSmoothing * Time.fixedDeltaTime);
