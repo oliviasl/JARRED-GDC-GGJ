0ef1c67a0a95247f2c495a2a835628702b9766ce
oliviasl <oslai@usc.edu>
2026-03-10 00:31:44 -0700

    Fail state countdown

--- Assets/Scripts/FailState.cs
+++ Assets/Scripts/FailState.cs
@@ -1,11 +1,14 @@
-using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 
 public class FailState : MonoBehaviour
 {
     [SerializeField] private GameObject failCamera;
     [SerializeField] private HealthController playerHealthController;
+    [SerializeField] private TextMeshProUGUI failCountdown;
 
     public UnityEvent onFailState;
 
@@ -22,6 +25,20 @@ public class FailState : MonoBehaviour
     private void Fail()
     {
         failCamera.SetActive(true);
+        failCountdown.gameObject.SetActive(true);
+        StartCoroutine(FailCountdown());
+        
         onFailState.Invoke();
+    }
+
+    IEnumerator FailCountdown()
+    {
+        yield return new WaitForSeconds(2f);
+        for (int i = 5; i > 0; --i)
+        {
+            failCountdown.text = "FAIL! Respawn in " + i;
+            yield return new WaitForSeconds(1f);
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
